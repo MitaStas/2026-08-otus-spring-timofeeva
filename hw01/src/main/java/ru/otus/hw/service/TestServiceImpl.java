763831ff68cef7b8a386e7f43e5266fc9aa2b20c
stastim <35169290+MitaStas@users.noreply.github.com>
2026-09-15 22:10:03 +0400

    Выполнение домашней работы №1 (#1)
    * выполнение дз № 1

---
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public void executeTest() {
+        printTestIntroduction();
+        var questions = questionDao.findAll();
+        printAllQuestions(questions);
+    }
+
+    private void printTestIntroduction() {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+    }
+
+    private void printAllQuestions(List<Question> questions) {
+        questions.forEach(this::printQuestion);
+    }
+
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        ioService.printLine("Choose number of answer:");
+        for (int i = 0; i < question.answers().size(); i++) {
+            ioService.printFormattedLine("%s. %s", i + 1, question.answers().get(i).text());
+        }
+        ioService.printLine("");
+    }
+}
